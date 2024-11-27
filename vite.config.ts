d0c98bff4be8e4376eaad8447e7479f7b0852665
jcasimiro <j.casimiro@qstrike.com>
2024-11-28 03:11:10 +0800

    Revert "Install shadcn-ui, add react plugin to vite.config.ts"
    This reverts commit 1cd0287dcbc15be8d4839dff572dbd400f6918a1.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-});
+  plugins: [vue()],
+})
