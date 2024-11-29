463ce3e51296fcb85035c304ccec20854f3b6f7b
jcasimiro <j.casimiro@qstrike.com>
2024-11-29 15:06:58 +0800

    Configure tailwindcss
    - Add jit to tailwindcss mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   plugins: [vue(), react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
 });
