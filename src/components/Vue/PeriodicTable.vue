a82dc3d22fec9052b626c901b415e54dc7b75172
jcasimiro <j.casimiro@qstrike.com>
2024-12-02 04:15:26 +0800

    Add on hover event on elements

--- src/components/Vue/PeriodicTable.vue
+++ src/components/Vue/PeriodicTable.vue
@@ -1102,18 +1102,18 @@ const elements: Element[] = [
         :class="[
           'border rounded p-4 text-center text-xs relative',
           element.group === 'nonmetal'
-            ? 'bg-blue-500 text-white'
+            ? 'bg-blue-500 hover:bg-blue-600 transition-all text-white'
             : element.group === 'alkaliMetal'
-            ? 'bg-green-500 text-white'
+            ? 'bg-green-500 hover:bg-green-600 transition-all text-white'
             : element.group === 'alkalineEarth'
-            ? 'bg-purple-500 text-white'
+            ? 'bg-purple-500 hover:bg-purple-600 transition-all text-white'
             : element.group === 'metalloid'
-            ? 'bg-yellow-500 text-black'
+            ? 'bg-yellow-500 hover:bg-yellow-600 transition-all text-black'
             : element.group === 'halogen'
-            ? 'bg-pink-500 text-white'
+            ? 'bg-pink-500 hover:bg-pink-600 transition-all text-white'
             : element.group === 'nobleGas'
-            ? 'bg-orange-500 text-white'
-            : 'bg-gray-200',
+            ? 'bg-orange-500 hover:bg-orange-600 transition-all text-white'
+            : 'bg-gray-200 hover:bg-gray-300 transition-all',
         ]"
       >
         <div class="absolute top-0 left-1 text-[10px] font-bold">
